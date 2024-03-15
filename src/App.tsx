45463d07fa833b1d50354c217ee4b7bf094f0467
esrafilelahi <essinho1121@gmail.com>
2024-03-15 17:36:59 +0330

    install packages and use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import { Button } from "esrafil-btn-component";
+import { Button as AButton } from "esrafil-btn-component";
+import { Button } from "./components/Button";
 
 function App() {
   return (
     <>
       <Button onClick={() => console.log("clicked me!")}>click me</Button>
+      <AButton onClick={() => console.log("AButton clicked me!")}>
+        click me
+      </AButton>
     </>
   );
 }
