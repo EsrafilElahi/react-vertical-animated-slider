845dd9299952720c5b12c3ebd958671335ba7925
esrafilelahi <essinho1121@gmail.com>
2024-04-19 20:06:38 +0330

    add carousel css file

--- src/components/VerticalAnimatedCarousel.tsx
+++ src/components/VerticalAnimatedCarousel.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import RightArrowIcon from "./RightArrowIcon";
 import LeftArrowIcon from "./LeftArrowIcon";
-import './index.css'
+import "../carousel.css";
 
 interface Props {
   data: unknown;
