6adcc51662294961ad82b1af6557a28c3f46ffa7
esrafilelahi <essinho1121@gmail.com>
2024-04-19 14:08:28 +0330

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import "./App.css";
 import { Button as AButton } from "esrafil-btn-component";
 import { Button } from "./components/Button";
 
 function App() {
   return (
-    <>
-      <Button onClick={() => console.log("clicked me!")}>click me</Button>
-      <AButton onClick={() => console.log("AButton clicked me!")}>
-        click me
-      </AButton>
-    </>
+    <div className="w-full h-full">
+      <div className="w-full h-full flex justify-center flex-col gap-10">
+        <div>
+          <span className="text-[#d0c]">asdasd</span>
+        </div>
+        <div>
+          <Button onClick={() => console.log("clicked me!")}>click me</Button>
+          <AButton onClick={() => console.log("AButton clicked me!")}>
+            click me
+          </AButton>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
