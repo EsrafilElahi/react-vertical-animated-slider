541ddd016e47febabd0d0e2880088968bd7d3a1f
esrafilelahi <essinho1121@gmail.com>
2024-04-19 18:30:49 +0330

    remove all unused app

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,5 @@
-// import VerticalAnimatedCarousel from "./components/VerticalAnimatedCarousel";
-// import VerticalCarouselItem from "./components/VerticalCarouselItem";
-// import { VerticalAnimatedCarousel } from "react-vertical-animated-slider";
-
 function App() {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     name: "name 1",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "name 2",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "name 3",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "name 4",
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "name 5",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "name 6",
-  //   },
-  //   {
-  //     id: 7,
-  //     name: "name 7",
-  //   },
-  // ];
-
-  return (
-    <div className="w-full h-full">
-      <div className="w-[30rem] mx-auto">
-        {/* <VerticalAnimatedCarousel data={data} CardItem={VerticalCarouselItem} /> */}
-      </div>
-    </div>
-  );
+  return <div className="w-full h-full"></div>;
 }
 
 export default App;
