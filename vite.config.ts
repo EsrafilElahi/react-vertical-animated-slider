ce0b1cd151b06afcf96fcd97650ab98e0148d01e
esrafilelahi <essinho1121@gmail.com>
2024-04-19 23:07:45 +0330

    version

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.js"),
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
       name: "react-vertical-animated-slider",
       fileName: (format) => `index.${format}.js`,
     },
