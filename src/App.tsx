d04d95b9fe9e5b470ffd4d1c9eca9d38f8efc4bf
esrafilelahi <essinho1121@gmail.com>
2024-04-20 23:37:20 +0330

    complete App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,66 @@
-import VerticalAnimatedCarousel from "./components/VerticalAnimatedCarousel";
-import styles from "./CardItem.module.css";
+// import VerticalAnimatedCarousel from "./components/VerticalAnimatedCarousel";
+// import styles from "./CardItem.module.css";
 
-const CardItem = ({ card }) => {
-  return (
-    <div className={styles.cardItem}>
-      <div className={styles.imageContainer}>
-        <img
-          src={"https://fakeimg.pl/300/"}
-          alt="user"
-          className={styles.image}
-        />
-      </div>
+// const CardItem = ({ card }) => {
+//   return (
+//     <div className={styles.cardItem}>
+//       <div className={styles.imageContainer}>
+//         <img
+//           src={"https://fakeimg.pl/300/"}
+//           alt="user"
+//           className={styles.image}
+//         />
+//       </div>
 
-      <div className={styles.textContainer}>
-        <span className={styles.id}>{card?.id}</span>
-        <span className={styles.name}>{card?.name}</span>
-      </div>
-    </div>
-  );
-};
+//       <div className={styles.textContainer}>
+//         <span className={styles.id}>{card?.id}</span>
+//         <span className={styles.name}>{card?.name}</span>
+//       </div>
+//     </div>
+//   );
+// };
 
 function App() {
-  const data = [
-    {
-      id: 1,
-      name: "name 1",
-    },
-    {
-      id: 2,
-      name: "name 2",
-    },
-    {
-      id: 3,
-      name: "name 3",
-    },
-    {
-      id: 4,
-      name: "name 4",
-    },
-    {
-      id: 5,
-      name: "name 5",
-    },
-    {
-      id: 6,
-      name: "name 6",
-    },
-    {
-      id: 7,
-      name: "name 7",
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     name: "name 1",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "name 2",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "name 3",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "name 4",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "name 5",
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "name 6",
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "name 7",
+  //   },
+  // ];
 
   return (
     <div className="w-full h-full">
       <div className="w-[30rem] mx-auto">
-        <VerticalAnimatedCarousel
+        {/* <VerticalAnimatedCarousel
           data={data}
           CardItem={CardItem}
           cardItemStyle={{ height: "10rem" }}
           // cardItemClassName="w-30"
-        />
+        /> */}
       </div>
     </div>
   );
